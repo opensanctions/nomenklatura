23c37efdb51ca7fb04327bcea6f82a68895fef1d
Friedrich Lindenberg <friedrich@pudo.org>
2012-07-15 12:59:47 +0200

    Basic handling of new dataset form

---
+++ linkspotting/util.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+import json
+
+from werkzeug.exceptions import NotFound
+from formencode.variabledecode import NestedVariables
+from flask import Response, current_app, request
+
+MIME_TYPES = {
+        'text/html': 'html',
+        'application/xhtml+xml': 'html',
+        'application/json': 'json',
+        'text/javascript': 'json',
+        }
+
+def request_format(request):
+    """ 
+    Determine the format of the request content. This is slightly 
+    ugly as Flask has excellent request handling built in and we 
+    begin to work around it.
+    """
+    return MIME_TYPES.get(request.content_type, 'html')
+
+def request_content(request):
+    """
+    Handle a request and return a generator which yields all rows 
+    in the incoming set.
+    """
+    format = request_format(request)
+    if format == 'json':
+        return json.loads(request.data)
+    else:
+        nv = NestedVariables()
+        return nv.to_python(request.form)
+
+class JSONEncoder(json.JSONEncoder):
+    """ This encoder will serialize all entities that have a to_dict
+    method by calling that method and serializing the result. """
+
+    def encode(self, obj):
+        if hasattr(obj, 'to_dict'):
+            obj = obj.to_dict()
+        return super(JSONEncoder, self).encode(obj)
+
+    def default(self, obj):
+        if hasattr(obj, 'to_dict'):
+            return obj.to_dict()
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        raise TypeError("%r is not JSON serializable" % obj)
+
+def jsonify(obj, status=200, headers=None):
+    """ Custom JSONificaton to support obj.to_dict protocol. """
+    return Response(json.dumps(obj, cls=JSONEncoder), headers=headers,
+                    status=status, mimetype='application/json')
+
+# quite hackish:
+def _response_format_from_path(request):
+    # This means: using <format> for anything but dot-notation is really 
+    # a bad idea here. 
+    adapter = current_app.create_url_adapter(request)
+    try:
+        return adapter.match()[1].get('format')
+    except NotFound:
+        return None
+
+def response_format():
+    """  Use HTTP Accept headers (and suffix workarounds) to 
+    determine the representation format to be sent to the client.
+    """
+    fmt = _response_format_from_path(request)
+    if fmt in MIME_TYPES.values():
+        return fmt
+    neg = request.accept_mimetypes.best_match(MIME_TYPES.keys())
+    return MIME_TYPES.get(neg)
+
--- linkspotting/web.py
+++ linkspotting/web.py
@@ -1,6 +1,45 @@
-from flask import render_template
+from flask import render_template, Response
+from formencode import Invalid
 
 from linkspotting.core import app, db
+from linkspotting.util import jsonify, response_format
+
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(410)
+@app.errorhandler(500)
+def handle_exceptions(exc):
+    """ Re-format exceptions to JSON if accept requires that. """
+    format = response_format()
+    if format == 'json':
+        body = {'status': exc.code,
+                'name': exc.name,
+                'description': exc.get_description(request.environ)}
+        return jsonify(body, status=exc.code,
+                       headers=exc.get_headers(request.environ))
+    return exc
+
+@app.errorhandler(Invalid)
+def handle_invalid(exc):
+    format = response_format()
+    if format == 'json':
+        body = {'status': 400,
+                'name': 'Invalid Data',
+                'description': unicode(exc),
+                'errors': exc.unpack_errors()}
+        return jsonify(body, status=400)
+    return Response(repr(exc.unpack_errors()), status=400, 
+                    mimetype='text/plain')
+
+
+@app.route('/new', methods=['GET'])
+def dataset_new():
+    return render_template('dataset/new.html')
+
+@app.route('/new', methods=['POST'])
+def dataset_create():
+    return render_template('dataset/new.html')
 
 @app.route('/')
 def index():
