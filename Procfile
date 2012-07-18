5ef7fc5ba51ca6eabfcd065c812e131b832c77f7
Friedrich Lindenberg <friedrich@pudo.org>
2012-07-18 14:36:05 +0200

    Try to work out heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python nomenklatura/manage.py runserver
---
+++ heroku_settings.py
@@ -0,0 +1,10 @@
+import os
+
+#DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
+GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
+
+
