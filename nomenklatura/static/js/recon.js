0472f91264aa77cf9096b1d2c01b818278a50721
Friedrich Lindenberg <friedrich@pudo.org>
2013-02-28 17:06:08 +0100

    Javascript bindings for recon screen.

---
+++ nomenklatura/static/js/recon.js
@@ -0,0 +1,54 @@
+$(function() {
+  
+  $saveForm = $('#save-match');
+  $radios = $('#save-match input[type=radio]');
+
+  $('#new-text').focus(function(e) {
+    $('#new').prop('checked', true);
+  });
+
+  $('input').keydown(function(e) {
+    if (e.which == 27) {
+      $(e.target).blur();
+    }
+  });
+
+  $('body').keydown(function(e) {
+    if (!$('input').is(':focus')) {
+      console.log(e.which);
+      var selectedRadio = $radios.index($radios.filter(':checked'));
+      console.log($radios.get(selectedRadio));
+
+      if (e.which >= 49 && e.which <= 57) {
+        $($radios.get(e.which-49)).prop('checked', true);
+        $saveForm.submit();
+        e.preventDefault();
+      }
+      switch(e.which) {
+        case 13: //Enter
+          $saveForm.submit();
+          break;
+        case 73: // 'i'
+          $('#invalid').prop('checked', true);
+          $saveForm.submit();
+          break;
+        case 78: // 'n'
+          $('#new-text').focus();
+          e.preventDefault();
+          break;
+        case 70: // 'f'
+          $('#filter-field').focus();
+          e.preventDefault();
+          break;
+        case 38: // up
+          $($radios.get(selectedRadio-1)).prop('checked', true);
+          e.preventDefault();
+          break;
+        case 40: // down;
+          $($radios.get(selectedRadio+1)).prop('checked', true);
+          e.preventDefault();
+          break;
+      }
+    }
+  });
+});
