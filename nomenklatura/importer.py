d67f3720bded41ba94b10d9a3887c39c7b86141a
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-18 21:21:03 +0200

    Clean up importer a tiny but.

--- nomenklatura/importer.py
+++ nomenklatura/importer.py
@@ -9,13 +9,17 @@ from messytables import headers_guess, headers_processor, \
 
 from nomenklatura.core import s3, app, db, celery
 from nomenklatura.exc import NotFound
+from nomenklatura.util import flush_cache
 from nomenklatura.model import Dataset, Entity, Alias, Account
+
 
 def get_bucket():
     return s3.create_bucket(app.config['S3_BUCKET'])
 
+
 def key_name(dataset, sig):
     return "%s/%s" % (dataset.name, sig)
+
 
 def upload_file(dataset, file_):
     fn = file_.filename.encode('ascii', 'replace')
@@ -29,11 +33,13 @@ def upload_file(dataset, file_):
     os.unlink(tmp)
     return sig
 
+
 def get_key(dataset, sig):
     key = get_bucket().get_key(key_name(dataset, sig))
     if not key:
         raise NotFound()
     return key
+
 
 def get_map_metadata(dataset, sig):
     metadata, row_set = parse_upload(dataset, sig)
@@ -46,6 +52,7 @@ def get_map_metadata(dataset, sig):
         'filename': metadata.get('filename')
         }
 
+
 def parse_upload(dataset, sig):
     key = get_key(dataset, sig)
     fn, ext = os.path.splitext(key.metadata.get('filename'))
@@ -54,6 +61,7 @@ def parse_upload(dataset, sig):
             mimetype=key.metadata.get('mimetype'),
             extension=ext[1:])
     return key.metadata, table_set.tables[0]
+
 
 def detect_headers(row_set):
     offset, headers = headers_guess(row_set.sample)
@@ -64,7 +72,7 @@ def detect_headers(row_set):
 
 @celery.task
 def import_upload(dataset_name, sig, account_id,
-        entity_col, alias_col):
+                  entity_col, alias_col):
     dataset = Dataset.find(dataset_name)
     account = Account.by_id(account_id)
     metadata, row_set = parse_upload(dataset, sig)
@@ -73,7 +81,7 @@ def import_upload(dataset_name, sig, account_id,
         data = dict([(c.column, c.value) for c in row])
         entity = data.pop(entity_col) if entity_col else None
         alias = data.pop(alias_col) if alias_col else None
-        if alias_col:
+        if alias_col and alias != entity:
             d = {'name': alias, 'data': data}
             alias_obj = Alias.lookup(dataset, d, account,
                                      match_entity=False)
@@ -82,10 +90,9 @@ def import_upload(dataset_name, sig, account_id,
             d = {'name': entity, 'data': data}
             entity_obj = Entity.by_name(dataset, entity)
             if entity_obj is None:
-                entity_obj = Entity.create(dataset,
-                        d, account)
+                entity_obj = Entity.create(dataset, d, account)
             entity_obj.data = data
         if alias_col and entity_col:
             alias_obj.match(dataset, {'choice': entity_obj.id}, account)
-        db.session.commit()
-
+    db.session.commit()
+    flush_cache()
