8fa07824e55e814daff38d121f78f0d0e1e25a6b
Friedrich Lindenberg <friedrich@pudo.org>
2013-11-18 20:53:58 +0100

    Remove old, unneeded functions.

--- nomenklatura/manage.py
+++ nomenklatura/manage.py
@@ -6,17 +6,12 @@ from nomenklatura import views
 
 manager = Manager(app)
 
+
 @manager.command
 def createdb():
     """ Make the database. """
     db.create_all()
 
-@manager.command
-def updb():
-    """ Update the database. """
-    db.engine.execute('ALTER TABLE "link" ADD COLUMN "data" TEXT')
-    db.engine.execute("""UPDATE "link" SET "data" = '{}'""")
-    db.session.commit()
 
 @manager.command
 def flush(dataset):
@@ -25,26 +20,6 @@ def flush(dataset):
         db.session.delete(alias)
     db.session.commit()
 
-@manager.command
-def dedup(dataset):
-    ds = Dataset.by_name(dataset)
-    from time import time
-    from nomenklatura.matching import match
-    begin = time()
-    for value in Value.all(ds).limit(20):
-        matches = match(value.value, ds)
-        matches = filter(lambda (c,v,s): v!=value.id, matches)
-        print [value.value, '=?', matches[0][1], matches[0][2]]
-    print "Time: %.2fms" % ((time() - begin)*1000)
-
-@manager.command
-def cleanup():
-    """ Clean up the database. """
-    for dataset in Dataset.all():
-        for alias in Alias.all_unmatched(dataset):
-            db.session.delete(alias)
-        db.session.flush()
-    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
