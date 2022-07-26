47f08fbaafb343eb159d09b6e66d537ce96f687f
Friedrich Lindenberg <friedrich@pudo.org>
2022-07-26 22:58:47 +0200

    Bump version: 2.5.2 → 2.5.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="nomenklatura",
-    version="2.5.2",
+    version="2.5.3",
     description="Make record linkages in followthemoney data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
