a5640941a81379256ed5c258af9b047f54ec958f
Friedrich Lindenberg <friedrich@pudo.org>
2023-03-23 13:37:32 +0100

    don't pre-load all endpoints

--- nomenklatura/enrich/yente.py
+++ nomenklatura/enrich/yente.py
@@ -34,7 +34,8 @@ class YenteEnricher(Enricher):
         if self._api_key is not None:
             self.session.headers["Authorization"] = f"ApiKey {self._api_key}"
 
-        self.cache.preload(f"{self._api}%")
+        match_url = urljoin(self._api, f"match/{self._dataset}")
+        self.cache.preload(f"{match_url}%")
 
     def make_url(self, entity: CE) -> str:
         return urljoin(self._api, f"entities/{entity.id}")
