30dbcfed8b4899af4b2368f9048291a1cd9316be
Friedrich Lindenberg <friedrich@pudo.org>
2023-03-24 14:27:46 +0100

    draft

---
+++ nomenklatura/publish/edges.py
@@ -0,0 +1,19 @@
+# Problem: undirected relationships in which both
+# entities are given as the source AND the target
+from nomenklatura.entity import CE
+from nomenklatura.resolver import Identifier
+
+
+def simplify_undirected(entity: CE):
+    if (
+        not entity.schema.edge_source
+        or not entity.schema.edge_target
+        or entity.schema.edge_directed
+    ):
+        return entity
+    source_ids = entity.get(entity.schema.edge_source)
+    target_ids = entity.get(entity.schema.edge_target)
+    if len(source_ids) < 2 and len(target_ids) < 2:
+        return entity
+    sources = [Identifier.get(s) for s in source_ids]
+    targets = [Identifier.get(s) for s in target_ids]
