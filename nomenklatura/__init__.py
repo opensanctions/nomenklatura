5c02e8a1207708e2e044b79122a3550ccfe2227e
Friedrich Lindenberg <friedrich@pudo.org>
2023-03-28 18:40:01 +0200

    Bump version: 2.8.2 → 2.9.0

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -4,7 +4,7 @@ from nomenklatura.resolver import Resolver
 from nomenklatura.loader import Loader, FileLoader, MemoryLoader
 from nomenklatura.index import Index
 
-__version__ = "2.8.2"
+__version__ = "2.9.0"
 __all__ = [
     "Dataset",
     "CompositeEntity",
