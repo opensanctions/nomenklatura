a9c2b204f1c1286ba3ffbdcfaf71b81d7b3c7c53
Friedrich Lindenberg <friedrich@pudo.org>
2023-05-03 18:11:10 +0200

    work on matcher registry

--- nomenklatura/matching/__init__.py
+++ nomenklatura/matching/__init__.py
@@ -1,21 +1,28 @@
+from typing import List, Type, TypeVar
 from nomenklatura.matching.v1.model import MatcherV1
 from nomenklatura.matching.v1.train import train_matcher as train_v1_matcher
 from nomenklatura.matching.v2.model import MatcherV2
 from nomenklatura.matching.v2.train import train_matcher as train_v2_matcher
 from nomenklatura.matching.ofac import OFAC249Matcher, OFAC249QualifiedMatcher
+from nomenklatura.matching.types import ScoringAlgorithm
 
-ALGORITHMS = [
-    "MatcherV1",
-    "MatcherV2",
-    "OFAC249Matcher",
-    "OFAC249QualifiedMatcher",
+# SA = TypeVar("SA", bound="Sco")
+
+ALGORITHMS: List[Type[ScoringAlgorithm]] = [
+    MatcherV1,
+    MatcherV2,
+    OFAC249Matcher,
+    OFAC249QualifiedMatcher,
 ]
+
+DefaultMatcher = MatcherV2
 
 __all__ = [
     "MatcherV1",
     "train_v1_matcher",
     "MatcherV2",
     "train_v2_matcher",
+    "DefaultMatcher",
     "OFAC249Matcher",
     "OFAC249QualifiedMatcher",
 ]
