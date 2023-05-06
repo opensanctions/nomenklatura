42379c9b1daadcb5465cbb57fbc991cacfc585ef
Friedrich Lindenberg <friedrich@pudo.org>
2023-05-06 17:37:58 +0200

    Bump version: 2.10.1 → 2.11.0

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -4,7 +4,7 @@ from nomenklatura.resolver import Resolver
 from nomenklatura.loader import Loader, FileLoader, MemoryLoader
 from nomenklatura.index import Index
 
-__version__ = "2.10.1"
+__version__ = "2.11.0"
 __all__ = [
     "Dataset",
     "CompositeEntity",
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="nomenklatura",
-    version="2.10.1",
+    version="2.11.0",
     description="Make record linkages in followthemoney data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
