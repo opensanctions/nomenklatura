80858829294dd2d8b11e5fc1e2babd39fcdb6269
JD Bothma <jbothma@gmail.com>
2023-11-14 17:27:43 +0200

    Emit matching securities

--- nomenklatura/enrich/openfigi.py
+++ nomenklatura/enrich/openfigi.py
@@ -2,6 +2,8 @@ import os
 import logging
 from typing import Any, Generator, Dict, List
 from urllib.parse import urljoin
+from followthemoney.util import make_entity_id
+from normality import slugify
 
 from nomenklatura.entity import CE
 from nomenklatura.dataset import DS
@@ -9,6 +11,7 @@ from nomenklatura.cache import Cache
 from nomenklatura.enrich.common import Enricher, EnricherConfig
 
 log = logging.getLogger(__name__)
+
 URL = "https://api.openfigi.com/v3/search"
 
 
@@ -17,38 +20,58 @@ class OpenFIGIEnricher(Enricher):
 
     def __init__(self, dataset: DS, cache: Cache, config: EnricherConfig):
         super().__init__(dataset, cache, config)
-        
+
         api_key = os.environ.get("OPENFIGI_API_KEY")
         self.session.headers["X-OPENFIGI-APIKEY"] = api_key
 
-    def make_entity_id(self, name):
-        return make_entity_id("name", name, )
+    def make_company_id(self, name):
+        return f"figi-co-{make_entity_id(name)}"
 
+    def make_security_id(self, figi):
+        return f"figi-id-{slugify(figi, sep='-')}"
+
+    def search(self, query):
+        body = {"query": query}
+        next = None
+
+        while True:
+            if next is not None:
+                body["start"] = next
+
+            log.info(f"Searching {query}. Offset={next}")
+            cache_key = f"{URL}:{query}:{next}"
+            resp = self.http_post_json_cached(URL, cache_key, body)
+            yield from resp["data"]
+
+            next = resp.get("next", None)
+            if next is None:
+                break
 
     def match(self, entity: CE) -> Generator[CE, None, None]:
-        for name in entity.get("name"):                        
-            body = {"query": name}
-            next = None
-
-            while True:
-                if next is not None:
-                    body["start"] = next
-
-                log.info(f"Searching {name}. Offset={next}")
-                cache_key = f"{URL}:{name}:{next}"
-                resp = self.http_post_json_cached(URL, cache_key, body)
-
-                for match in resp.get("data"):
-                    print(name, "->", match["name"])
-                    other = self.make_entity(entity, "Company")
-                    other.id = self.make_entity_id(match["name"])
-                    other.add("name", match["name"])
-                    yield other
-
-                next = resp.get("next", None)
-                if next is None:
-                    break
-
+        for name in entity.get("name"):
+            for match in self.search(name):
+                other = self.make_entity(entity, "Company")
+                name = match.get("name", None)
+                if name is None:
+                    continue
+                other.id = self.make_company_id(name)
+                other.add("name", name)
+                yield other
 
     def expand(self, entity: CE, match: CE) -> Generator[CE, None, None]:
-       pass 
+        yield match
+
+        name = match.get("name")[0]
+        for item in self.search(name):
+            if item["name"] != name:
+                continue
+
+            security = self.make_entity(match, "Security")
+            security.id = self.make_security_id(item["figi"])
+            security.add("name", item["figi"])
+            security.add("issuer", match)
+            security.add("ticker", item["ticker"])
+            security.add("type", item["securityType"])
+            security.add("notes", f'exchange {item["exchCode"]}')
+
+            yield security
