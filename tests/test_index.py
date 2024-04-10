02895efac1f2a42c91e17e06513efd944c08798b
JD Bothma <jbothma@gmail.com>
2024-04-10 12:55:40 +0100

    Add tests for Index.match

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
+from nomenklatura.dataset import Dataset
+from nomenklatura.entity import CompositeEntity
 from nomenklatura.index import Index
+from nomenklatura.resolver.identifier import Identifier
 from nomenklatura.store import SimpleMemoryStore
 
 DAIMLER = "66ce9f62af8c7d329506da41cb7c36ba058b3d28"
+VERBAND_ID = "62ad0fe6f56dbbf6fee57ce3da76e88c437024d5"
+VERBAND_BADEN_ID = "69401823a9f0a97cfdc37afa7c3158374e007669"
+VERBAND_BADEN_DATA = {
+    "id": "bla",
+    "schema": "Company",
+    "properties": {
+        "name": ["VERBAND DER METALL UND ELEKTROINDUSTRIE BADEN WURTTEMBERG"]
+    },
+}
 
 
 def test_index_build(dstore: SimpleMemoryStore):
@@ -45,3 +57,41 @@ def test_index_pairs(dstore: SimpleMemoryStore, dindex: Index):
     # assert "Schnabel" in (entity0.caption, entity1.caption)
     assert score > 0
     # assert False
+
+
+def test_match_score(dstore: SimpleMemoryStore, dindex: Index):
+    """Match an entity that isn't itself in the index"""
+    dx = Dataset.make({"name": "test", "title": "Test"})
+    entity = CompositeEntity.from_data(dx, VERBAND_BADEN_DATA)
+    matches = dindex.match(entity)
+    # 9 entities in the index where some token in the query entity matches some
+    # token in the index.
+    assert len(matches) == 9, matches
+
+    top_result = matches[0]
+    assert top_result[0] == Identifier(VERBAND_BADEN_ID), top_result
+    assert 1.99 < top_result[1] < 2, top_result
+
+    next_result = matches[1]
+    assert next_result[0] == Identifier(VERBAND_ID), next_result
+    assert 1.66 < next_result[1] < 1.67, next_result
+
+    match_identifiers = set(str(m[0]) for m in matches)
+    assert VERBAND_ID in match_identifiers  # validity
+    assert DAIMLER in dindex.entities
+    assert DAIMLER not in match_identifiers
+
+
+def test_top_match_matches_strong_pairs(dstore: SimpleMemoryStore, dindex: Index):
+    """Pairs with high scores are each others' top matches"""
+
+    view = dstore.default_view()
+    strong_pairs = [p for p in dindex.pairs() if p[1] > 3.0]
+    assert len(strong_pairs) > 4
+
+    for pair, pair_score in strong_pairs:
+        entity = view.get_entity(pair[0])
+        matches = dindex.match(entity)
+        # it'll match itself and the other in the pair
+        for match, match_score in matches[:2]:
+            assert match in pair, (match, pair)
