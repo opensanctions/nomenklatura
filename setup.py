c4c785b45610b9bb7a012bfaa874d95a5c8faa4b
JD Bothma <jbothma@gmail.com>
2024-06-13 16:22:06 +0100

    Bump version: 3.11.5 → 3.12.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="nomenklatura",
-    version="3.11.5",
+    version="3.12.0",
     description="Make record linkages in followthemoney data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
