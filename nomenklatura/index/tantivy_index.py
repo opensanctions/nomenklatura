40dd146784643fd12eab11326419ba53bd718f75
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-08-03 16:26:16 +0200

    sketching

--- nomenklatura/index/tantivy_index.py
+++ nomenklatura/index/tantivy_index.py
@@ -2,11 +2,10 @@ import logging
 from normality import WS
 from pathlib import Path
 from rigour.ids import StrictFormat
-from followthemoney.types import registry
-from typing import Any, Dict, List, Tuple, Generator, Set
-from tantivy import Query, Occur, Index, SchemaBuilder, Document
-import math
 from collections import defaultdict
+from followthemoney.types import registry
+from typing import Any, Dict, Iterable, List, Tuple, Generator, Set
+from tantivy import Query, Occur, Index, SchemaBuilder, Document
 
 from nomenklatura.dataset import DS
 from nomenklatura.entity import CE
@@ -17,25 +16,22 @@ from nomenklatura.index.common import BaseIndex
 
 log = logging.getLogger(__name__)
 
-INDEX_IGNORE = (
-    registry.entity,
-    registry.url,
-    registry.json,
-    registry.html,
-    registry.language,
-    registry.mimetype,
-    registry.checksum,
-    # TODO: should topics be a field so that PEPs, sanctioned entities are more
-    # easily found in xref?
-    registry.topic,
-)
 FULL_TEXT = {
-    # registry.text,
-    registry.string,
+    registry.name,
     registry.address,
     registry.identifier,
-    # registry.email,
+    registry.text,
+    registry.string,
 }
+INDEXED_TYPES = FULL_TEXT.union(
+    {
+        registry.date,
+        registry.phone,
+        registry.email,
+        # TODO: should topics be a field so that PEPs, sanctioned entities are more
+        # easily found in xref?
+    }
+)
 BOOST_NAME_PHRASE = 4.0
 BOOSTS = {
     registry.name.name: 4.0,
@@ -44,6 +40,31 @@ BOOSTS = {
     registry.address.name: 3.0,
     registry.identifier.name: 5.0,
 }
+TEXT = registry.text.name
+
+
+def split_tokens(value: str) -> Iterable[str]:
+    return (v for v in value.split(WS) if len(v) > 1)
+
+
+def ngram(text: str, length: int = 3) -> List[str]:
+    return [text]
+    if len(text) <= length:
+        return [text]
+    model: List[str] = []
+    # model will contain n-gram strings
+    count = 0
+    for token in text[: len(text) - length + 1]:
+        model.append(text[count : count + length])
+        count = count + 1
+    return model
+
+
+def split_ngrams(text: str, grams: int = 3) -> List[str]:
+    ngrams: List[str] = []
+    for token in text.split(WS):
+        ngrams.extend(ngram(token, grams))
+    return ngrams
 
 
 class TantivyIndex(BaseIndex[DS, CE]):
@@ -61,9 +82,9 @@ class TantivyIndex(BaseIndex[DS, CE]):
         schema_builder.add_text_field("entity_id", tokenizer_name="raw", stored=True)
         schema_builder.add_text_field("schemata", tokenizer_name="raw")
         schema_builder.add_text_field(registry.name.name)
-        schema_builder.add_text_field(registry.email.name)
         schema_builder.add_text_field(registry.address.name)
-        schema_builder.add_text_field(registry.text.name)
+        schema_builder.add_text_field(TEXT)
+        schema_builder.add_text_field(registry.email.name, tokenizer_name="raw")
         schema_builder.add_text_field(registry.identifier.name, tokenizer_name="raw")
         schema_builder.add_text_field(registry.iban.name, tokenizer_name="raw")
         schema_builder.add_text_field(registry.phone.name, tokenizer_name="raw")
@@ -81,7 +102,62 @@ class TantivyIndex(BaseIndex[DS, CE]):
             self.build_index = True
 
     @classmethod
-    def entity_fields(cls, entity: CE) -> Generator[Tuple[str, Set[str]], None, None]:
+    def index_fields(cls, entity: CE) -> Generator[Tuple[str, List[str]], None, None]:
+        """
+        A generator of each
+
+        - index field name and
+        -  the set of normalised but not tokenised values for that field
+
+        for the given entity.
+        """
+        fields: Dict[str, List[str]] = defaultdict(list)
+
+        for prop, value in entity.itervalues():
+            type = prop.type
+            if type not in INDEXED_TYPES or not prop.matchable:
+                continue
+
+            if type in FULL_TEXT:
+                fields[TEXT].append(value)
+
+            if type == registry.name:
+                fields[type.name].extend(ngram(value.lower()))
+                norm = fingerprint_name(value)
+                if norm is not None:
+                    fields[type.name].extend(ngram(norm))
+                continue
+
+            if type == registry.date:
+                if len(value) > 4:
+                    fields[type.name].append(value[:4])
+                fields[type.name].append(value[:10])
+                continue
+
+            if type == registry.identifier:
+                clean_id = StrictFormat.normalize(value)
+                if clean_id is not None:
+                    fields[type.name].append(clean_id)
+                continue
+
+            if type == registry.address:
+                cleaned = clean_text_basic(value)
+                if cleaned is not None:
+                    fields[type.name].append(cleaned)
+                continue
+
+            if prop.matchable and type in (
+                registry.phone,
+                registry.email,
+                registry.country,
+            ):
+                fields[type.name].append(value)
+
+        print(fields)
+        yield from fields.items()
+
+    @classmethod
+    def query_fields(cls, entity: CE) -> Generator[Tuple[str, Set[str]], None, None]:
         """
         A generator of each
 
@@ -93,27 +169,25 @@ class TantivyIndex(BaseIndex[DS, CE]):
         fields: Dict[str, Set[str]] = defaultdict(set)
 
         for prop, value in entity.itervalues():
-            type = prop.type
-            if type in INDEX_IGNORE:
+            if not prop.matchable and not prop.name == "weakAlias":
                 continue
 
-            if type in FULL_TEXT:
-                fields[registry.text.name].add(value.lower())
+            type = prop.type
 
             if type == registry.name:
-                fields[type.name].add(value.lower())
+                fields[type.name].update(split_tokens(value.lower()))
                 norm = fingerprint_name(value)
                 if norm is not None:
-                    fields[type.name].add(norm)
+                    fields[type.name].update(split_tokens(norm))
                 continue
 
-            if type == registry.date and prop.matchable:
+            if type == registry.date:
                 if len(value) > 4:
                     fields[type.name].add(value[:4])
                 fields[type.name].add(value[:10])
                 continue
 
-            if type == registry.identifier and prop.matchable:
+            if type == registry.identifier:
                 clean_id = StrictFormat.normalize(value)
                 if clean_id is not None:
                     fields[type.name].add(clean_id)
@@ -122,7 +196,7 @@ class TantivyIndex(BaseIndex[DS, CE]):
             if type == registry.address and prop.matchable:
                 cleaned = clean_text_basic(value)
                 if cleaned is not None:
-                    fields[type.name].add(cleaned)
+                    fields[type.name].update(split_tokens(cleaned))
                 continue
 
             if prop.matchable and type in (
@@ -139,43 +213,56 @@ class TantivyIndex(BaseIndex[DS, CE]):
         """
         A generator of queries for the given index field and set of values.
         """
-        # Name phrase
-        if field == registry.name.name:
-            for value in values:
-                words = value.split(WS)
-                word_count = len(words)
-                if word_count > 1:
-                    slop = math.ceil(2 * math.log(word_count))
-                    yield Query.boost_query(
-                        Query.phrase_query(self.schema, field, words, slop),  # type: ignore
-                        BOOST_NAME_PHRASE,
-                    )
+        # # Name phrase
+        # if field == registry.name.name:
+        #     for value in values:
+        #         words = value.split(WS)
+        #         word_count = len(words)
+        #         if word_count > 1:
+        #             slop = math.ceil(2 * math.log(word_count))
+        #             yield Query.boost_query(
+        #                 Query.phrase_query(self.schema, field, words, slop),  # type: ignore
+        #                 BOOST_NAME_PHRASE,
+        #             )
 
         # Any of set of tokens in all values of the field
-        if field in {
-            registry.address.name,
-            registry.name.name,
-            registry.text.name,
-            registry.string.name,
-        }:
-            word_set: Set[str] = set()
-            for value in values:
-                word_set.update(value.split(WS))
-            term_queries: List[Query] = []
-            for word in word_set:
-                term_queries.append(Query.term_query(self.schema, field, word))
+        if field in {registry.address.name, registry.name.name}:
+            # word_set: Set[str] = set()
+            # for value in values:
+            #     word_set.update(value.split(WS))
+            # term_queries: List[Query] = []
             yield Query.boost_query(
-                Query.boolean_query([(Occur.Should, q) for q in term_queries]),
+                Query.term_set_query(self.schema, field, list(values)),
                 BOOSTS.get(field, 1.0),
             )
+            # for word in values:
+            #     for ng in ngram(word):
+            #         yield Query.boost_query(
+            #             Query.term_query(self.schema, field, ng),
+            #             BOOSTS.get(field, 1.0),
+            #         )
+            #     # query = Query.term_query(self.schema, field, word)
+            #     # term_queries.append(Query.term_query(self.schema, field, word))
+            #     # yield Query.boost_query(
+            #     #     query,
+            #     #     BOOSTS.get(field, 1.0),
+            #     # )
+            #     # yield Query.term_query(self.schema, TEXT, word)
+
             return
 
         # entire value as a term
-        for value in values:
-            yield Query.boost_query(
-                Query.term_query(self.schema, field, value),
-                BOOSTS.get(field, 1.0),
-            )
+        if field in {
+            registry.email.name,
+            registry.identifier.name,
+            registry.phone.name,
+        }:
+            for value in values:
+                yield Query.boost_query(
+                    Query.term_query(self.schema, field, value),
+                    BOOSTS.get(field, 1.0),
+                )
+                # yield Query.term_query(self.schema, TEXT, value)
 
     def entity_query(self, entity: CE) -> Query:
         schema_query = Query.term_query(self.schema, "schemata", entity.schema.name)
@@ -183,9 +270,10 @@ class TantivyIndex(BaseIndex[DS, CE]):
         if entity.id is not None:
             id_query = Query.term_query(self.schema, "entity_id", entity.id)
             queries.append((Occur.MustNot, id_query))
-        for field, value in self.entity_fields(entity):
+        for field, value in self.query_fields(entity):
             for query in self.field_queries(field, value):
                 queries.append((Occur.Should, query))
+        # print(entity.id, repr(entity.caption), len(queries))
         return Query.boolean_query(queries)
 
     def build(self) -> None:
@@ -205,7 +293,7 @@ class TantivyIndex(BaseIndex[DS, CE]):
             idx += 1
             schemata = [s.name for s in entity.schema.matchable_schemata]
             document = Document(entity_id=entity.id, schemata=schemata)
-            for field, values in self.entity_fields(entity):
+            for field, values in self.index_fields(entity):
                 for value in values:
                     document.add_text(field, value)
             writer.add_document(document)
