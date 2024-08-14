d01f8cb8d675af05c57eae663a21ba660169e20e
JD Bothma <jbothma@gmail.com>
2024-08-14 14:51:30 +0100

    Try middle value for missing data

--- nomenklatura/matching/compare/countries.py
+++ nomenklatura/matching/compare/countries.py
@@ -2,7 +2,7 @@ from followthemoney.proxy import E
 from followthemoney.types import registry
 
 from nomenklatura.matching.util import type_pair
-from nomenklatura.matching.compare.util import is_disjoint
+from nomenklatura.matching.compare.util import is_disjoint, has_overlap
 
 
 def country_mismatch(query: E, result: E) -> float:
@@ -14,4 +14,7 @@ def country_mismatch(query: E, result: E) -> float:
 def country_overlap(query: E, result: E) -> float:
     """Both entities are linked to the same country."""
     qv, rv = type_pair(query, result, registry.country)
-    return 1.0 if has_overlap(qv, rv) else 0.0
+    if qv and rv:
+        return 1.0 if has_overlap(qv, rv) else 0.0
+    else:
+        return 0.5
