084527b0d94221751445356fc8e68c024f7d501f
JD Bothma <jbothma@gmail.com>
2024-11-21 13:20:01 +0000

    Remove index selection, add slim enricher base

--- nomenklatura/index/common.py
+++ nomenklatura/index/common.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Generic, List, Tuple
+from typing import Generic, Iterable, List, Tuple
 from nomenklatura.resolver import Identifier
 from nomenklatura.dataset import DS
 from nomenklatura.entity import CE
@@ -18,7 +18,7 @@ class BaseIndex(Generic[DS, CE]):
 
     def pairs(
         self, max_pairs: int = MAX_PAIRS
-    ) -> List[Tuple[Tuple[Identifier, Identifier], float]]:
+    ) -> Iterable[Tuple[Tuple[Identifier, Identifier], float]]:
         raise NotImplementedError
 
     def match(self, entity: CE) -> List[Tuple[Identifier, float]]:
