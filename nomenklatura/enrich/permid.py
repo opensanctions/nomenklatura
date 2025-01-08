c3bd37a7553f8100c15443b314a6d1952899cec7
JD Bothma <jbothma@gmail.com>
2025-01-08 16:56:31 +0000

    Retry unretried errors from permid
    Removing superfluous retry code - normally retries should be
    handled by urllib3 Retry.

--- nomenklatura/enrich/permid.py
+++ nomenklatura/enrich/permid.py
@@ -1,24 +1,22 @@
-import io
 import csv
+import io
 import json
 import logging
-from lxml import etree
-
-# from pprint import pprint
-from itertools import product
 from functools import lru_cache
+from itertools import product
 from typing import cast, Set, Generator, Optional, Dict, Any
 from urllib.parse import urljoin
+
 from followthemoney.types import registry
+from lxml import etree
 from requests import Session
 
-from nomenklatura.entity import CE
-from nomenklatura.dataset import DS
 from nomenklatura.cache import Cache
+from nomenklatura.dataset import DS
 from nomenklatura.enrich.common import Enricher, EnricherConfig
 from nomenklatura.enrich.common import EnrichmentAbort
+from nomenklatura.entity import CE
 from nomenklatura.util import fingerprint_name
-
 
 log = logging.getLogger(__name__)
 
@@ -186,7 +184,6 @@ class PermIDEnricher(Enricher[DS]):
                 cache_key,
                 data=query,
                 headers=headers,
-                retry=0,
                 cache_days=self.cache_days,
             )
             seen_matches: Set[str] = set()
