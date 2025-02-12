d5dde1c833229648ab9f3c488c330827bcf21691
JD Bothma <jbothma@gmail.com>
2025-02-12 13:01:44 +0000

    Clean up unnecessary load journey

--- nomenklatura/resolver/resolver.py
+++ nomenklatura/resolver/resolver.py
@@ -377,7 +377,6 @@ class Resolver(Linker[CE]):
         judgement: Judgement,
         user: Optional[str] = None,
         score: Optional[float] = None,
-        timestamp: Optional[str] = None,
     ) -> Identifier:
         edge = self.get_edge(left_id, right_id)
         if edge is None:
@@ -397,7 +396,7 @@ class Resolver(Linker[CE]):
                 return canonical
 
         edge.judgement = judgement
-        edge.timestamp = timestamp or utc_now().isoformat()[:28]
+        edge.timestamp = utc_now().isoformat()[:28]
         edge.user = user or getpass.getuser()
         edge.score = score or edge.score
         self._register(edge)
@@ -497,11 +496,6 @@ class Resolver(Linker[CE]):
                 if not line:
                     break
                 edge = Edge.from_line(line)
-                # There exist legacy positive edges which don't lead to a canonical.
-                # If we load these edges using self.decide, some of them generate
-                # canonicals which are greater than the current canonicals connected
-                # to these edges. This could imply lots of unplanned rekeying.
-                # So let's just register the edges as they are for now.
                 self._register(edge)
         self._invalidate()
 
