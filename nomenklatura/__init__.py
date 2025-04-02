6ac4afde5dccc83eb90a40e93a6ffc8a02941d99
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-04-02 18:29:49 +0200

    Bump version: 3.17.1 → 3.17.2

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -4,7 +4,7 @@ from nomenklatura.resolver import Resolver
 from nomenklatura.store import Store, View
 from nomenklatura.index import Index
 
-__version__ = "3.17.1"
+__version__ = "3.17.2"
 __all__ = [
     "Dataset",
     "CompositeEntity",
