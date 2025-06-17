c874eb4f4f023387441fb7708c8b6b5d27af8c6a
JD Bothma <jbothma@gmail.com>
2025-06-17 12:07:31 +0100

    Bump version: 3.17.3 → 3.17.4

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -4,7 +4,7 @@ from nomenklatura.resolver import Resolver
 from nomenklatura.store import Store, View
 from nomenklatura.index import Index
 
-__version__ = "3.17.3"
+__version__ = "3.17.4"
 __all__ = [
     "Dataset",
     "CompositeEntity",
