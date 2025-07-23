0abbab7d2ec8c46e17f4ac043fbd38d65ac04fa4
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-07-23 18:21:15 +0200

    Update lv2 identifiers spec

--- nomenklatura/matching/logic_v2/identifiers.py
+++ nomenklatura/matching/logic_v2/identifiers.py
@@ -6,6 +6,8 @@ from followthemoney.property import Property
 from followthemoney.types import registry
 from followthemoney.proxy import EntityProxy
 
+from nomenklatura.matching.types import FtResult, ScoringConfig
+
 
 def _format_normalize(
     format: Type[IdentifierFormat], entity: EntityProxy, prop: Property
@@ -20,7 +22,7 @@ def _format_normalize(
 
 def _identifier_format_match(
     format_name: str, query: EntityProxy, result: EntityProxy
-) -> float:
+) -> FtResult:
     """Check if the identifier format is the same for two entities."""
     schema = model.common_schema(query.schema, result.schema)
     format = get_identifier_format(format_name)
@@ -40,57 +42,82 @@ def _identifier_format_match(
         if prop.format is not None and get_identifier_format(prop.format) == format:
             query_format.update(query_values)
             result_format.update(result_values)
-    if len(query_format.intersection(result_identifiers)) > 0:
-        return 1.0
-    if len(result_format.intersection(query_identifiers)) > 0:
-        return 1.0
-    if len(query_identifiers.intersection(result_identifiers)) > 0:
-        return 0.8
-    return 0.0
+    left_common = query_format.intersection(result_identifiers)
+    if len(left_common) > 0:
+        detail = f"Matched {format.TITLE}: {', '.join(left_common)}"
+        return FtResult(score=1.0, detail=detail)
+    right_common = result_format.intersection(query_identifiers)
+    if len(right_common) > 0:
+        detail = f"Matched {format.TITLE}: {', '.join(right_common)}"
+        return FtResult(score=1.0, detail=detail)
+    non_common = query_identifiers.intersection(result_identifiers)
+    if len(non_common) > 0:
+        detail = f"Out-of-format match: {', '.join(non_common)}"
+        return FtResult(score=0.8, detail=detail)
+    return FtResult(score=0.0, detail=f"No {format.TITLE} match")
 
 
-def lei_code_match(query: EntityProxy, result: EntityProxy) -> float:
+def lei_code_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two entities have the same Legal Entity Identifier."""
     return _identifier_format_match("lei", query, result)
 
 
-def bic_code_match(query: EntityProxy, result: EntityProxy) -> float:
+def bic_code_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two entities have the same SWIFT BIC."""
     return _identifier_format_match("bic", query, result)
 
 
-def ogrn_code_match(query: EntityProxy, result: EntityProxy) -> float:
+def ogrn_code_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two entities have the same Russian company registration (OGRN) code."""
     return _identifier_format_match("ogrn", query, result)
 
 
-def inn_code_match(query: EntityProxy, result: EntityProxy) -> float:
+def inn_code_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two entities have the same Russian tax identifier (INN)."""
     return _identifier_format_match("inn", query, result)
 
 
-def uei_code_match(query: EntityProxy, result: EntityProxy) -> float:
+def uei_code_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two entities have the same US Unique Entity ID (UEI)."""
     return _identifier_format_match("uei", query, result)
 
 
-def npi_code_match(query: EntityProxy, result: EntityProxy) -> float:
+def npi_code_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two entities have the same US National Provider Identifier (NPI)."""
     return _identifier_format_match("npi", query, result)
 
 
-def isin_security_match(query: EntityProxy, result: EntityProxy) -> float:
+def isin_security_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two securities have the same ISIN."""
     # if not has_schema(query, result, "Security"):
     #     return 0.0
     return _identifier_format_match("isin", query, result)
 
 
-def vessel_imo_mmsi_match(query: EntityProxy, result: EntityProxy) -> float:
+def vessel_imo_mmsi_match(
+    query: EntityProxy, result: EntityProxy, config: ScoringConfig
+) -> FtResult:
     """Two vessels have the same IMO or MMSI identifier."""
-    imo_score = _identifier_format_match("imo", query, result)
-    if imo_score > 0.0:
-        return imo_score
+    imo_res = _identifier_format_match("imo", query, result)
+    if imo_res.score > 0.0:
+        return imo_res
     query_mmsis = query.get("mmsi", quiet=True)
     result_mmsis = result.get("mmsi", quiet=True)
-    return registry.identifier.compare_sets(query_mmsis, result_mmsis)
+    score = registry.identifier.compare_sets(query_mmsis, result_mmsis)
+    if score > 0.0:
+        return FtResult(score=score, detail="MMSI match")
+    return FtResult(score=score, detail="No IMO or MMSI match")
