b774b18728806b9498fd739a0a6bbd60e3f0cee6
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-09-13 15:33:53 +0200

    Clean up a bit

--- nomenklatura/matching/logic_v2/names/util.py
+++ nomenklatura/matching/logic_v2/names/util.py
@@ -44,40 +44,21 @@ class Match:
 
     def __str__(self) -> str:
         """String representation of the Match object for debugging."""
-        explanation = None
-
         qps_str = " ".join([part.comparable for part in self.qps])
         rps_str = " ".join([part.comparable for part in self.rps])
         if self.symbol is not None:
-            explanation = f"{qps_str}≈{rps_str} symbolMatch {self.symbol}"
+            explanation = f"{qps_str!r}≈{rps_str!r} symbolMatch({self.symbol})"
         elif not len(qps_str):
-            explanation = f"{rps_str} extraResultPart"
+            explanation = f"{rps_str!r} extraResultPart"
         elif not len(rps_str):
-            explanation = f"{qps_str} extraQueryPart"
+            explanation = f"{qps_str!r} extraQueryPart"
         elif qps_str == rps_str:
-            explanation = f"{rps_str} literalMatch"
+            explanation = f"{rps_str!r} literalMatch"
         else:
-            explanation = f"{qps_str}≈{rps_str} fuzzyMatch"
+            explanation = f"{qps_str!r}≈{rps_str!r} fuzzyMatch"
         # score_str = f"{self.score:.2f}".lstrip("0")
         # return f"[{qps_str}<{score_str}>{rps_str}]"
-        return f"[{explanation} {self.score:.2f}, weight {self.weight:.2f}]"
-    
-
-    # def get_human_readable_explanation(self) -> str:
-    #     """Get a human-readable explanation of the match."""
-    #     qps_str = " ".join([part.comparable for part in self.qps])
-    #     rps_str = " ".join([part.comparable for part in self.rps])
-
-    #     if self.symbol is not None:
-    #         return f"[{qps_str}] matched [{rps_str}] using symbol {str(self.symbol)}"
-    #     if not len(qps_str):
-    #         return f"Unmatched result part: [{rps_str}]"
-    #     if not len(rps_str):
-    #         return f"Unmatched query part: [{qps_str}]"
-    #     if qps_str == rps_str:
-    #         return f"Exact match: [{rps_str}]"
-    #     score_str = f"{self.score:.2f}".lstrip("0")
-    #     return f"[{qps_str}] matched [{rps_str}] with score <{score_str}>"
+        return f"[{explanation}: {self.score:.2f}, weight {self.weight:.2f}]"
 
 
 NUMERIC = re.compile(r"\d{1,}")
