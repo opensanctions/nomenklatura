9fbdd754c315602b4b8bdaa943d5847b6cae654e
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-10-29 20:48:06 +0100

    Simplify identifier matching code; remove out-of-format matching

--- contrib/name_benchmark/performance.py
+++ contrib/name_benchmark/performance.py
@@ -54,7 +54,7 @@ def run_benchmark() -> None:
     config = LogicV2.default_config()
     func = LogicV2.compare
     print("Running benchmark for: %s" % (func.__name__))
-    for i in range(100):
+    for i in range(1000):
         for check in checks:
             func(check.query, check.candidate, config)
 
