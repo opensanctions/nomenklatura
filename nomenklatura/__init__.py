3b0e1b17c095a37909281228931d866c8156974d
JD Bothma <jbothma@gmail.com>
2025-11-17 12:34:05 +0000

    Bump version: 4.2.0 → 4.3.0

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -2,5 +2,5 @@ from nomenklatura.resolver import Resolver, Linker
 from nomenklatura.store import Store, View
 from nomenklatura.judgement import Judgement
 
-__version__ = "4.2.0"
+__version__ = "4.3.0"
 __all__ = ["Resolver", "Store", "View", "Judgement", "Linker"]
