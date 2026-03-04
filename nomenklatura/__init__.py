1a11d9788bf83801d5c7115a0f8f293c70f36939
Leon Handreke <leon@opensanctions.org>
2026-03-04 14:58:03 +0100

    some pre-commit reformatting

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -1,6 +1,6 @@
-from nomenklatura.resolver import Resolver, Linker
-from nomenklatura.store import Store, View
 from nomenklatura.judgement import Judgement
+from nomenklatura.resolver import Linker, Resolver
+from nomenklatura.store import Store, View
 
 __version__ = "4.7.0"
 __all__ = ["Resolver", "Store", "View", "Judgement", "Linker"]
