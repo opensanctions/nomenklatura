c3b6a4e6da315e7329be1cf81ae61a96630106f9
Friedrich Lindenberg <friedrich@opensanctions.org>
2026-07-02 18:32:46 +0200

    Bump version: 4.11.1 → 4.11.2

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -2,5 +2,5 @@ from nomenklatura.judgement import Judgement
 from nomenklatura.resolver import Linker, Resolver
 from nomenklatura.store import Store, View
 
-__version__ = "4.11.1"
+__version__ = "4.11.2"
 __all__ = ["Resolver", "Store", "View", "Judgement", "Linker"]
