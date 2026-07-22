fd7fc92164ab00f0abb7a3854ef90dddf9321226
JD Bothma <jd@opensanctions.org>
2026-07-22 11:47:28 +0100

    Bump version: 4.12.2 → 4.12.3

--- nomenklatura/__init__.py
+++ nomenklatura/__init__.py
@@ -2,5 +2,5 @@ from nomenklatura.judgement import Judgement
 from nomenklatura.resolver import Linker, Resolver
 from nomenklatura.store import Store, View
 
-__version__ = "4.12.2"
+__version__ = "4.12.3"
 __all__ = ["Resolver", "Store", "View", "Judgement", "Linker"]
